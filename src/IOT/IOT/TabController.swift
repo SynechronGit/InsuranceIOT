548c562d4c3523c776fbd737bc9d015942317d0b
Rupendra <rupendra@example.com>
2017-02-28 16:19:15 +0530

    Initial import.

---
+++ src/IOT/IOT/TabController.swift
@@ -0,0 +1,23 @@
+//
+//  TabController.swift
+//  IOT
+//
+//  Created by rupendra on 2/28/17.
+//  Copyright © 2017 Synechron. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UIViewController {
+    @IBOutlet weak var temperatureContainerView: UIView!
+    @IBOutlet weak var gpsContainerView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.temperatureContainerView.isHidden = false
+        self.gpsContainerView.isHidden = true
+    }
+    
+}
---
+++ src/IOT/IOT/TemperatureController.swift
@@ -0,0 +1,35 @@
+//
+//  TemperatureController.swift
+//  IOT
+//
+//  Created by rupendra on 2/28/17.
+//  Copyright © 2017 Synechron. All rights reserved.
+//
+
+import UIKit
+
+class TemperatureController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
